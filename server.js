918e9e73f452954df8570936df03c9927f4eee7b
Rashid <reshid.asedov@div.edu.az>
2025-07-29 20:05:10 +0400

    novbeti-commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
 server.use('', router)
-server.listen(process.env.PORT || 10000 || 6000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(process.env.PORT, () => {
+  console.log(`JSON Server is running on port ${process.env.PORT}`);
+});
